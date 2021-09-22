ebea72a399bf8e70e034fe5d90fc835428c75d79
nilton.schumacher <nilton.schumacher@alta-rt.com>
2021-09-22 20:21:51 -0300

    Finished the addToCart and the Home page

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -34,9 +34,32 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const addProduct = async (productId: number) => {
     try {
-    
+			const updatedCart = [...cart];
+			const productExists = updatedCart.find(product => product.id === productId);
+			const stock = await api.get(`/stock/${productId}`);
+			const stockAmount = stock.data.amount;
+			const currentAmount = productExists ? productExists.amount : 0;
+			const amount = currentAmount + 1;
+
+			if (amount > stockAmount) {
+				toast.error('Quantidade solicitada fora de estoque');
+				return;
+			}
+
+			if (productExists) {
+				productExists.amount = amount;
+			} else {
+				const product = await api.get(`/stock/${productId}`);
+				const newProduct = {
+					...product.data,
+					amount: 1
+				}
+				updatedCart.push(newProduct);
+			}
+			setCart(updatedCart);
+			localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
@@ -53,7 +76,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      console.log(productId);
+      
     } catch {
       // TODO
     }
